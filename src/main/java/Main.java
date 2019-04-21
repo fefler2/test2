da4d25a6498754411354c51c6bcf3de333599aec
giciarz412 <tomaszfefler1990@gmail.com>
2019-04-21 15:31:58 +0200

    very simple web browser - first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main extends JPanel{
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("VerySimpleWebBrowser");
+
+
+    }
+}
