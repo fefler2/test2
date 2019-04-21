c4863083fb1660722b55c659cd0fe51685755890
giciarz412 <tomaszfefler1990@gmail.com>
2019-04-21 15:36:59 +0200

    very simple web browser 2

---
+++ src/main/java/WebBroswerMain.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class WebBroswerMain extends JPanel{
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("VerySimpleWebBrowser");
+        WebBroswerMain webBroswerMain = new WebBroswerMain();
+
+        window.setContentPane(webBroswerMain);
+        window.setSize(1200, 900);
+
+    }
+
+    public WebBroswerMain(){
+        setBackground(Color.LIGHT_GRAY);
+    }
+}
